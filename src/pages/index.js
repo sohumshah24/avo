cd17dee4204fce4d8ce8a21b22d5074db10521dc
sohumshah24 <sohumshah24@gmail.com>
2020-02-07 21:59:03 +0000

    Upgrades

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
 import React from "react"
 
-export default () => <div>Hello Sexy!</div>
+export default () => (
+  <div style={{ color: `purple` }}>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+  </div>
+)
